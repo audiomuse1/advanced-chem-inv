7973346f81529028db2ed1a9a75881deb782a387
Andrew Hartford <andrewhartford@utexas.edu>
2018-03-04 16:02:10 -0600

    new update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://audiomuse:Nbs76gf$@ds227168.mlab.com:27168/aca-heroku");
 app.use(bodyParser.json());
 
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI);
+
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Listening on port:${port}`);
