080b586b49b67e1f6804a7519b6cc434722224bb
Andrew Hartford <andrewhartford@utexas.edu>
2018-03-04 16:28:52 -0600

    added react-redux dependency

--- dist/index.js
+++ dist/index.js
@@ -25,6 +25,9 @@ _mongoose2.default.Promise = global.Promise;
 _mongoose2.default.connect("mongodb://audiomuse:Nbs76gf$@ds227168.mlab.com:27168/aca-heroku");
 app.use(_bodyParser2.default.json());
 
+var MONGODB_URI = process.env.MONGODB_URI;
+_mongoose2.default.connect(MONGODB_URI);
+
 var port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log("Listening on port:" + port);
